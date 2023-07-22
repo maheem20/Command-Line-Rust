ddab3f6e319cf13aa7cf8c4a4cbeee6220ea0d99
maheem_20 <mwistanbuly@gmail.com>
2023-07-22 23:24:48 +0200

    create a Command to run hello
    in current crate

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -2,7 +2,7 @@ use assert_cmd::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
+    let mut cmd = Command::cargo_bin("hello").unwrap();
     let res = cmd.output();
     assert!(res.is_ok());
 }
