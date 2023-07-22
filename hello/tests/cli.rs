657720b6f6e6b444d45c1db7b841be0c23a5590a
maheem_20 <mwistanbuly@gmail.com>
2023-07-22 23:25:50 +0200

    use Assert::success
    to ensure command succeeded

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -3,6 +3,5 @@ use assert_cmd::Command;
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
-    let res = cmd.output();
-    assert!(res.is_ok());
+    cmd.assert().success();
 }
